f8c4337512a6a210a68a6557bddc47f05303e0f1
中島真一 <nakajimabb@gmail.com>
2020-07-23 21:19:36 +0900

    modify DictView component, add TextField for lemma

--- src/DictView.tsx
+++ src/DictView.tsx
@@ -1,10 +1,23 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Box, Checkbox, Grid, makeStyles } from '@material-ui/core';
-import { PinDrop, PinDropOutlined } from '@material-ui/icons';
+import React, { useEffect, useContext } from 'react';
+import {
+  Box,
+  Checkbox,
+  Grid,
+  IconButton,
+  TextField,
+  makeStyles,
+} from '@material-ui/core';
+import {
+  PinDrop,
+  PinDropOutlined,
+  ChevronLeft,
+  ChevronRight,
+} from '@material-ui/icons';
 import BibleReference from './BibleReference';
 import DictPassage from './DictPassage';
 import MorphPassage from './MorphPassage';
 import AppContext from './AppContext';
+import { shapeLemma } from './NodeObj';
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -37,6 +50,9 @@ const useStyles = makeStyles((theme) => ({
     dislay: 'flex',
     flexDirection: 'column',
   },
+  input: {
+    width: 70,
+  },
 }));
 
 interface DictViewProp {
@@ -62,39 +78,77 @@ const DictView: React.FC<DictViewProp> = ({ depth }) => {
     setTargetWords(words);
   };
 
+  const incLemma = (inc: number) => () => {
+    let words = [...targetWords];
+    const m = lemma.match(/(\d+)/);
+    if (m && m[1]) {
+      const number = +m[1] + inc;
+      if (number > 0) {
+        const newLemma = shapeLemma(String(number), lang);
+        words[depth] = { ...word, lemma: newLemma, text: '', morph: '' };
+        setTargetWords(words);
+      }
+    }
+  };
+
+  const changeLemma = (e: React.ChangeEvent<{ value: unknown }>) => {
+    let words = [...targetWords];
+    const newLemma = shapeLemma(String(e.currentTarget.value), lang);
+    words[depth] = { ...word, lemma: newLemma, text: '', morph: '' };
+    setTargetWords(words);
+  };
+
   return (
     <Box>
-      {wordText && lemma && (
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
-        >
-          <Grid item>
-            <span className={lang} style={{ fontSize: '180%' }}>
-              {wordText}
-            </span>
-            &nbsp;({lemma})
+      {lemma && (
+        <Box m={0}>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item>
+              <IconButton size="small" onClick={incLemma(-1)}>
+                <ChevronLeft />
+              </IconButton>
+              <TextField
+                value={lemma}
+                size="small"
+                onChange={changeLemma}
+                className={classes.input}
+              />
+              <IconButton size="small" onClick={incLemma(1)}>
+                <ChevronRight />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <BibleReference lemma={lemma} depth={depth + 1} />
+              <Checkbox
+                checked={fixed}
+                icon={<PinDropOutlined />}
+                checkedIcon={<PinDrop />}
+                className={classes.item}
+                onChange={reverseFixed}
+                style={{ marginLeft: 5 }}
+              />
+            </Grid>
           </Grid>
-          <Grid item>
-            <Checkbox
-              checked={fixed}
-              icon={<PinDropOutlined />}
-              checkedIcon={<PinDrop />}
-              className={classes.item}
-              onChange={reverseFixed}
-              style={{ textAlignLast: 'right' }}
-            />
-          </Grid>
-        </Grid>
+        </Box>
       )}
+
+      {wordText && lemma && (
+        <Box mb={0}>
+          <span className={lang} style={{ fontSize: '180%' }}>
+            {wordText}
+          </span>
+        </Box>
+      )}
+
       <Box mb={1}>
         <MorphPassage morph={morph} />
       </Box>
-      <Box mb={1}>
-        <BibleReference lemma={lemma} depth={depth + 1} />
-      </Box>
+
       <Box mb={1} className={classes.dict}>
         <DictPassage lemma={lemma} lang={lang} />
       </Box>
