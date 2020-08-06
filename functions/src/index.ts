00a0936500f2af47c7fa060aa2182c5e6c32e820
中島真一 <nakajimabb@gmail.com>
2020-08-06 23:07:34 +0900

    add Cloud Functions getAuthUserList

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,3 +19,65 @@ export const onCreateUser = functions
     });
     return batch.commit();
   });
+
+const authorized = async (
+  authType: 'anonymous' | 'authenticated' | 'admin',
+  context: functions.https.CallableContext
+) => {
+  if (authType === 'anonymous') {
+    return true;
+  } else {
+    if (context.auth) {
+      if (authType === 'authenticated') {
+        return true;
+      } else {
+        const uid = context.auth.uid;
+        const userRecord = await admin.auth().getUser(uid);
+        const claimAdmin = userRecord.customClaims?.admin;
+        if (authType === 'admin' && claimAdmin) {
+          return true;
+        } else {
+          throw new functions.https.HttpsError(
+            'permission-denied',
+            '権限がありません。'
+          );
+        }
+      }
+    } else {
+      throw new functions.https.HttpsError(
+        'unauthenticated',
+        'ログインしてください。'
+      );
+    }
+  }
+};
+
+// onCall function を返すデコレータ
+const callable = (
+  authType: 'anonymous' | 'authenticated' | 'admin',
+  func: (data: any, context: functions.https.CallableContext) => any
+) => {
+  return functions.region('asia-northeast1').https.onCall((data, context) => {
+    const f = async () => {
+      try {
+        const auth = await authorized(authType, context);
+        if (auth) {
+          const result = await func(data, context);
+          return result;
+        }
+      } catch (error) {
+        throw new functions.https.HttpsError(
+          error.code || 'unknown',
+          error.message || 'エラーが発生しました。',
+          error
+        );
+      }
+    };
+    return f();
+  });
+};
+
+exports.getAuthUserList = callable('admin', async (data, context) => {
+  const listUsersResult = await admin.auth().listUsers();
+  return listUsersResult.users;
+});
